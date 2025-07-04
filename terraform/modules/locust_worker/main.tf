c5ce182edac44c5add5f88c01c86326f37d1db58
KJR020 <johnjiro1114@gmail.com>
2025-07-05 04:57:41 +0900

    不要なファイルを削除

--- terraform/modules/locust_worker/main.tf
+++ terraform/modules/locust_worker/main.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "worker" {
   description = "Security group for Locust worker nodes"
   vpc_id      = var.vpc_id
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 5557
+    to_port     = 5558
+    cidr_blocks = ["10.0.0.0/16"]  // VPCのCIDRブロックに合わせて設定
+    description = "Locust master communication"
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
